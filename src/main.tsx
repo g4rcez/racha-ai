857297477e0451e9a4876f810630a939d9085149
g4rcez <allan.f.garcez@gmail.com>
2023-12-13 23:20:03 -0300

    wip: work in progress

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
 import { Layout } from "~/components/layout";
-import { noop } from "~/lib/fn";
 import { routerConfig } from "~/router";
 import { Preferences } from "~/store/preferences.store";
 import DefaultTheme from "~/styles/default.json";
@@ -30,7 +29,9 @@ async function start() {
     return document.getElementById("root")!;
 }
 
-const idleCallback = window.requestIdleCallback ?? noop;
+const idleCallback = window.requestIdleCallback ?? ((fn: Function) => fn());
+
+console.log(idleCallback);
 
 start().then((root) => {
     idleCallback(() =>
