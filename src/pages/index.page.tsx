0f25ea8dc859c5aa558f3517bec38993a351660d
g4rcez <allan.f.garcez@gmail.com>
2023-12-13 21:41:46 -0300

    feat: start landing-page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,16 @@
-import { UserForm } from "~/components/users/user-form";
+import { Link } from "brouther";
+import { link, links } from "~/router";
 
 export default function IndexPage() {
     return (
-        <div className="flex h-screen w-screen flex-col items-center justify-center px-12 align-middle text-2xl font-medium">
-            <UserForm />
+        <div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-l from-indigo-950 px-12 align-middle text-2xl font-medium">
+            <Link
+                href={link(links.app)}
+                className="flex flex-col transition-all duration-500 link:text-main-bg link:underline"
+            >
+                <h1 className="text-7xl font-bold leading-relaxed tracking-wide">Divide aí</h1>
+                <p className="-mt-2 text-lg">A melhor maneira de dividir sua conta com a galera</p>
+            </Link>
         </div>
     );
 }
