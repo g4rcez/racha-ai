0894f6163a7222c6f53aec7a5a49ec2715412320
g4rcez <allan.f.garcez@gmail.com>
2024-01-27 00:39:23 -0300

    wip: work in progress

--- next.config.js
+++ next.config.js
@@ -1,26 +1,12 @@
-const { withSentryConfig } = require("@sentry/nextjs");
-
-const withSerwist = require("@serwist/next").default({
-  swSrc: "./src/app/sw.ts",
-  swDest: "public/sw.js",
-  register: true,
-  reloadOnOnline: true,
-});
-
-const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  experimental: { swcTraceProfiling: true },
-  compiler: {
-    reactRemoveProperties: { properties: ["^data-test$", "^data-testid$"] },
+module.exports = require("@sentry/nextjs").withSentryConfig(
+  {
+    reactStrictMode: true,
+    poweredByHeader: false,
+    experimental: { swcTraceProfiling: true },
+    compiler: {
+      reactRemoveProperties: { properties: ["^data-test$", "^data-testid$"] },
+    },
   },
-};
-
-const envConfig =
-  process.env.NODE_ENV === "development" ? nextConfig : withSerwist(nextConfig);
-
-module.exports = withSentryConfig(
-  envConfig,
   { silent: true, org: "g4rcez", project: "racha-ai" },
   {
     widenClientFileUpload: true,
